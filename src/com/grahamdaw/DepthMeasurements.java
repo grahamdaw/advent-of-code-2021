6680dc013fa5b43440cf252c40190d7f43ddc3a8
Graham Daw <grahamdaw@users.noreply.github.com>
2021-12-01 21:56:04 +0200

    Day 1 part 2 solution

--- src/com/grahamdaw/DepthMeasurements.java
+++ src/com/grahamdaw/DepthMeasurements.java
@@ -26,8 +26,30 @@ public class DepthMeasurements {
     }
 
     private boolean haveWeGoneDeeper() {
-        Integer previous = measurements.get(max(0, measurements.size() - 2));
-        Integer current = measurements.get(measurements.size() - 1);
+        // Non-windowed checks (day 1 part 1)
+        // Integer previous = measurements.get(max(0, measurements.size() - 2));
+        // Integer current = measurements.get(measurements.size() - 1);
+
+        // Only start checks if we have enough data points to compare
+        if (measurements.size() < 4) {
+            return false;
+        }
+        // There is enough data in the array to start checking measurements
+        List<Integer> previousWindow = measurements.subList(
+                measurements.size() - 4,
+                measurements.size() - 1
+        );
+        List<Integer> currentWindow = measurements.subList(
+                measurements.size() - 3,
+                measurements.size()
+        );
+
+        Integer previous = previousWindow.stream().mapToInt(Integer::intValue).sum();
+        Integer current = currentWindow.stream().mapToInt(Integer::intValue).sum();
+
+        // System.out.println("Checking " + previous + " vs " + current);
+
+        // If needed can make this more complex to measure other types of changes (e.g. shallower, no-change)
         return isDeeper(previous, current);
 
     }

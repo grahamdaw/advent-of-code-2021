4c45c8cf457c54c046e92fd51c7ca79d4e77e0c7
Graham Daw <grahamdaw@users.noreply.github.com>
2021-12-01 21:33:28 +0200

    Day 1 part 1 solution

--- src/com/grahamdaw/DepthMeasurements.java
+++ src/com/grahamdaw/DepthMeasurements.java
@@ -8,19 +8,27 @@ import static java.lang.Math.max;
 public class DepthMeasurements {
 
     private final List<Integer> measurements = new ArrayList<>();
+    private int timesDeeper = 0;
 
-    static boolean isBigger(Integer prev, Integer next) {
+    static boolean isDeeper(Integer prev, Integer next) {
         return prev < next;
     }
 
     public void addMeasurement(Integer measurement) {
         measurements.add(measurement);
+        if (haveWeGoneDeeper()) {
+            timesDeeper++;
+        }
     }
 
-    public boolean currentlyGoingDeeper() {
+    public int getTimesDeeper() {
+        return timesDeeper;
+    }
+
+    private boolean haveWeGoneDeeper() {
         Integer previous = measurements.get(max(0, measurements.size() - 2));
         Integer current = measurements.get(measurements.size() - 1);
-        return isBigger(previous, current);
+        return isDeeper(previous, current);
 
     }
 }
--- src/com/grahamdaw/Main.java
+++ src/com/grahamdaw/Main.java
@@ -1,9 +1,39 @@
 package com.grahamdaw;
 
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class Main {
 
+    // The way we are selecting the solution for the day until I have time to make a better way
+    static Integer day = 1;
+    // Let's just consider the file is here
+    static String file = "data/depth-readings.txt";
+
     public static void main(String[] args) {
-	    // write your code here
-        System.out.println("hello");
+        try{
+            switch(day){
+                case 1:
+                    System.out.println("~~~ Day 1 solution ~~~");
+                    DepthMeasurements dm = new DepthMeasurements();
+                    getDepthReadings().forEach((line) -> {
+                        dm.addMeasurement(Integer.parseInt(line));
+                    });
+                    System.out.println("We have gone " + dm.getTimesDeeper() + " times deeper");
+                    break;
+                default:
+                    System.out.println("No solution for day " + day + " yet!");
+            }
+        } catch (Exception e) {
+            System.err.println("Well that didn't work...");
+            e.printStackTrace();
+        }
+    }
+
+    private static Stream<String> getDepthReadings() throws IOException {
+        return Files.lines(Paths.get(file));
     }
 }

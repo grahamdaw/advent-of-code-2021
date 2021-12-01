504fe3b91b3d4239c9f12cb3cc98694a46d60ce2
Graham Daw <grahamdaw@users.noreply.github.com>
2021-12-01 14:18:25 +0200

    Initial commit

---
+++ src/com/grahamdaw/Day1.java
@@ -0,0 +1,7 @@
+package com.grahamdaw;
+
+public class Day1 {
+    // Take input
+    // Do stuff
+    // output profit
+}
---
+++ src/com/grahamdaw/Main.java
@@ -0,0 +1,9 @@
+package com.grahamdaw;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello");
+    }
+}
